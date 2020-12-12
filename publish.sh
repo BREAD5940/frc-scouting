6eee7501bd7fdb19a02e5eacd6f21a4910d0f596
Annika <alarson23@dtechhs.org>
2020-12-11 18:09:13 -0800

    Improve the publishing script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
+# Publishes a new version of the frc-scouting package
+# Currently, only Annika has sufficient permissions to run this script
+# If you're someone else, you should ask her to publish it!
 
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+echo "Running tests and checks..."
 npm test
-cp src/games/deep-space.sql built/games/deep-space.sql
+echo "Publishing to NPM..."
 npm publish
+echo "Publishing to GitHub Packages..."
+npm publish --registry=https://npm.pkg.github.com
+echo "Done!"
