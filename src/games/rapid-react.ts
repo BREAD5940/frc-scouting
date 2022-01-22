c250a839aeebf34fea97bc6a728dc7d45c3eca9c
Annika Larson <alarson23@dtechhs.org>
2022-01-21 19:21:29 -0800

    Add some scoring functionality for Rapid React

--- src/games/rapid-react.ts
+++ src/games/rapid-react.ts
@@ -6,7 +6,7 @@
 
 // TODO: storage implementation (SQLite and possibly JSON) and point/RP calculation
 
-import {Alliance, Match, MatchData} from '..';
+import {Alliance, GamePieceTracker, Match, MatchData} from '..';
 
 /** Tracks the highest RUNG the robot reached */
 enum MonkeyBarState {
@@ -20,6 +20,39 @@ enum MonkeyBarState {
     Traversal = 5,
 }
 
+/** Tracks a robot's climb status */
+export class MonkeyBarTracker implements GamePieceTracker {
+    state: MonkeyBarState;
+
+    /** Creates a MonkeyBarTracker */
+    constructor(state: MonkeyBarState) {
+        this.state = state;
+    }
+
+    /** Gets the total number of points scored from climbing for this robot */
+    get totalPoints(): number {
+        switch (this.state) {
+        case MonkeyBarState.None: case MonkeyBarState.DidNotAttempt:
+            return 0;
+        case MonkeyBarState.Low:
+            return 1;
+        case MonkeyBarState.Mid:
+            return 2;
+        case MonkeyBarState.High:
+            return 3;
+        case MonkeyBarState.Traversal:
+            return 4;
+        default:
+            throw new Error(`Unexpected value for MonkeyBarState: ${this.state}`);
+        }
+    }
+
+    /** RP getter */
+    get rankingPoints(): number {
+        return 0;
+    }
+}
+
 interface Shots {
     high: {made: number, missed: number};
     low: {made: number, missed: number};
@@ -27,29 +60,89 @@ interface Shots {
 
 // TODO: should this/monkey bars be GamePieceTrackers?
 /** Tracks the robot's shots */
-interface ShotTracker {
+class ShotTracker implements GamePieceTracker {
     auto: Shots;
     teleop: Shots;
+
+    /** Creates a ShotTracker */
+    constructor(auto: Shots, teleop: Shots) {
+        this.auto = auto;
+        this.teleop = teleop;
+    }
+
+    /** Gets the total number of points scored via cargo */
+    get totalPoints(): number {
+        return (
+            this.auto.high.made * 4 +
+            this.auto.low.made * 2 +
+            this.teleop.high.made * 2 +
+            this.teleop.low.made
+        );
+    }
+
+    /** Gets the total number shots made */
+    get totalShotsMade(): number {
+        return (
+            this.auto.high.made +
+            this.auto.low.made +
+            this.teleop.high.made +
+            this.teleop.low.made
+        );
+    }
+
+    /** ranking point getters */
+    get rankingPoints(): number {
+        // Ranking points are calculated on a per-alliance basis in RR.
+        return 0;
+    }
 }
 
 interface RapidReactMatchData extends Partial<MatchData> {
-    shots: ShotTracker;
+    highShots: Shots;
+    lowShots: Shots;
     climbing: MonkeyBarState;
 }
 
 /** Represents one robot's performance in a match of Rapid React */
 export class RapidReactMatch extends Match {
-    shots: ShotTracker;
-    climbing: MonkeyBarState;
+    pieceTrackers: [ShotTracker, MonkeyBarTracker];
 
     /** creates a match */
     constructor(
         teamNumber: number, type: string, number: number, alliance: Alliance, data: RapidReactMatchData,
     ) {
         super(teamNumber, type, number, alliance, [], data);
-        this.shots = data.shots;
-        this.climbing = data.climbing;
+        this.pieceTrackers = [
+            new ShotTracker(data.highShots, data.lowShots),
+            new MonkeyBarTracker(data.climbing),
+        ];
     }
 }
 
+/**
+ * Since Rapid React's ranking points are based on the alliance's performance,
+ * rather than any individual robot, this function calculates an *alliance's* ranking points.
+ *
+ * `teams` should be an array of matches: one for each robot on the alliance in the given match
+ */
+export function allianceRapidReactRP(teams: RapidReactMatch[]) {
+    let rankingPoints = 0;
 
+    let totalCargo = 0;
+    let totalClimbPoints = 0;
+    let totalAutoCargo = 0;
+    for (const team of teams) {
+        totalCargo += team.pieceTrackers[0].totalShotsMade;
+        totalAutoCargo += team.pieceTrackers[0].auto.low.made + team.pieceTrackers[0].auto.high.made;
+        totalClimbPoints += team.pieceTrackers[1].totalPoints;
+    }
+    const quintet = totalAutoCargo >= 5;
+
+    // Cargo shots RP
+    if (totalCargo >= (quintet ? 18 : 20)) rankingPoints++;
+
+    // Climbing RP
+    if (totalClimbPoints >= 16) rankingPoints++;
+
+    return rankingPoints;
+}
