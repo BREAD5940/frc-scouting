04e9c11ef9cf44239b8c7036f1838fa24c7e3bd8
Annika Larson <alarson23@dtechhs.org>
2022-01-21 19:27:58 -0800

    Stub out tests for Rapid React point calculation

---
+++ src/games/rapid-react.test.ts
@@ -0,0 +1,22 @@
+/**
+ * Tests for Rapid React.
+ *
+ * TODO: test the storage backends (in backend.ts)
+ */
+
+describe('alliance Ranking Point calculation', () => {
+    test.todo('correctly tallies RP in general cases');
+
+    test.todo('works with one robot');
+    test.todo('works with two robots');
+
+    test.todo('accounts for the Quintet bonus');
+});
+
+describe('point calculation', () => {
+    test.todo('general test cases');
+    test.todo('doubles Auto cargo points');
+    test.todo('climbing points');
+    test.todo('crossing the start line');
+    test.todo('fouls assessed against the robot');
+});
